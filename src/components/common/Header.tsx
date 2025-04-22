84486383c3d179103e4631a9f0e4ac1e3109bd68
tora <styx.tora.works@gmail.com>
2025-04-23 02:14:09 +0900

    [update]loading button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,17 +1,19 @@
 'use client';
 
 import { createClient } from '@/app/lib/supabase/client';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useTransition } from 'react';
 import { usePathname } from 'next/navigation';
 import { signOut } from '@/app/lib/actions/auth';
 import Image from 'next/image';
 import { UserMetadata, Session } from '@supabase/supabase-js';
 import styles from './Header.module.scss';
+import { PulseLoader } from 'react-spinners';
 
 export default function Header() {
   const [session, setSession] = useState<Session | null>(null);
   const pathname = usePathname();
   const supabase = createClient();
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     const getUser = async () => {
@@ -22,6 +24,10 @@ export default function Header() {
     };
     getUser();
   }, []);
+
+  const handleSignOut = async () => {
+    startTransition(() => signOut());
+  };
 
   if (pathname === '/login' || !session?.user) {
     return null;
@@ -43,9 +49,18 @@ export default function Header() {
         <button
           type="button"
           className={styles['sign-out-btn']}
-          onClick={signOut}
+          onClick={handleSignOut}
+          disabled={isPending}
         >
-          Sign out
+          <span>Sign out</span>
+          {isPending && (
+            <PulseLoader
+              className={styles['loader']}
+              loading={isPending}
+              size={6}
+              color={`#7134eb`}
+            />
+          )}
         </button>
       </div>
     </header>
